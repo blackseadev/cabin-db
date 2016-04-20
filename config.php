313e494e35eee91ac702a75b496023acde0a1dee
blackseadev <leonardbira@gmail.com>
2016-04-20 10:33:00 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,10 +6,10 @@
 	(
 		'name'=>'Cabin DB',
 		'db_auto_connect' => false,
-		'db_username'=>'root',
-		'db_password'=>'',
+		'db_username'=>'forge',
+		'db_password'=>'Dg46RG525pZohN1yuYEb',
 		'db_host'=>'localhost',
-		'db_name'=>'',
+		'db_name'=>'forge',
 
 		// when set to auto, we dynamically work out what it is
 		'base_url' => 'auto',
